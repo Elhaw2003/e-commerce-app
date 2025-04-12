0b34f1e2c5e6c570e2a46aec43d7c58a98511114
Elhaw2003 <ebrahimelhaw0@gmail.com>
2025-04-12 14:45:57 +0200

    developer branch created

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class ECommerceApp extends StatelessWidget {
       builder: (context, child) {
         return MaterialApp(
           debugShowCheckedModeBanner: false,
+
         );
       },
     );

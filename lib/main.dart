1daa987a0cc8834564aa425612ed1930effb29b6
Elhaw2003 <ebrahimelhaw0@gmail.com>
2025-04-12 14:44:21 +0200

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main(){
+  runApp(const ECommerceApp());
+}
+class ECommerceApp extends StatelessWidget {
+  const ECommerceApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(390, 844),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+        );
+      },
+    );
+  }
+}
